554bddf40bf9f7533eebdd358ec85710725e10ec
Justinas <justasrimavicius@gmail.com>
2022-06-17 10:28:40 +0300

    Ship hit mechanics and tests.

--- src/index.js
+++ src/index.js
@@ -13,11 +13,28 @@ shipFactory.prototype.length=function(){
         return Math.abs(this.startCords[1]-this.endCords[1])+1;
     }
 };
-shipFactory.prototype.penis='penis';
-
-
-
-const ship1= new shipFactory([6,6],[6,1],[],false);
+shipFactory.prototype.isSunk=function(){
+    if(this.sunk==true){
+        this.sunk=true;
+        return true;
+    } else {
+        this.sunk=false;
+        return false;
+    }
+};
+shipFactory.prototype.isHit=function(coords){
+    if((coords[0]<=this.startCords[0]&&coords[0]>=this.endCords[0])||(coords[0]<=this.endCords[0]&&coords[0]>=this.startCords[0])){
+        if((coords[1]<=this.startCords[1]&&coords[1]>=this.endCords[1])||(coords[1]<=this.endCords[1]&&coords[1]>=this.startCords[1])){
+            this.hitPosition.push(coords);
+            return coords;
+        }
+        else{
+            return 'not hit';
+        }
+    } else {
+        return 'not hit';
+    }
+};
 
 
 module.exports={
--- src/index.test.js
+++ src/index.test.js
@@ -1,31 +1,69 @@
 const index=require('./index.js');
 const shipFactory=index.shipFactory;
 
-test('ship goes vertically',()=>{
-    const ship1=new shipFactory([6,6],[6,1],[],false);
-    expect(ship1.length()).toBe(6);
+describe('ship creation and length check',function(){
+// test('ship goes vertically',()=>{
+//     const ship1=new shipFactory([6,6],[6,1],[],false);
+//     expect(ship1.length()).toBe(6);
+// })
+// test('ship goes horizontally',()=>{
+//     const ship1=new shipFactory([6,6],[1,6],[],false);
+//     expect(ship1.length()).toBe(6);
+// })
+// test('ship goes vertically(end and start coords switched)',()=>{
+//     const ship1=new shipFactory([1,6],[6,6],[],false);
+//     expect(ship1.length()).toBe(6);
+// })
+// test('ship goes horizontally(end and start coords switched)',()=>{
+//     const ship1=new shipFactory([6,1],[6,6],[],false);
+//     expect(ship1.length()).toBe(6);
+// })
+// test('1 length ship',()=>{
+//     const ship1=new shipFactory([1,1],[1,1],[],false);
+//     expect(ship1.length()).toBe(1);
+// })
+// test('4 length ship vertically',()=>{
+//     const ship1=new shipFactory([4,4],[4,1],[],false);
+//     expect(ship1.length()).toBe(4);
+// })
+// test('4 length ship horizontally',()=>{
+//     const ship1=new shipFactory([4,4],[1,4],[],false);
+//     expect(ship1.length()).toBe(4);
+// })
 })
-test('ship goes horizontally',()=>{
-    const ship1=new shipFactory([6,6],[1,6],[],false);
-    expect(ship1.length()).toBe(6);
-})
-test('ship goes vertically(end and start coords switched)',()=>{
-    const ship1=new shipFactory([1,6],[6,6],[],false);
-    expect(ship1.length()).toBe(6);
-})
-test('ship goes horizontally(end and start coords switched)',()=>{
-    const ship1=new shipFactory([6,1],[6,6],[],false);
-    expect(ship1.length()).toBe(6);
-})
-test('1 length ship',()=>{
-    const ship1=new shipFactory([1,1],[1,1],[],false);
-    expect(ship1.length()).toBe(1);
-})
-test('4 length ship vertically',()=>{
-    const ship1=new shipFactory([4,4],[4,1],[],false);
-    expect(ship1.length()).toBe(4);
-})
-test('4 length ship horizontally',()=>{
-    const ship1=new shipFactory([4,4],[1,4],[],false);
-    expect(ship1.length()).toBe(4);
+
+describe('hit mechanics',function(){
+// test('hit mechanics - 4 length vertical',()=>{
+//     const ship1=new shipFactory([4,4],[4,1],[],false);
+//     expect(ship1.isHit([4,3])).toEqual([4,3]);
+// })
+// test('hit mechanics - 4 length horizontal',()=>{
+//     const ship1=new shipFactory([4,4],[1,4],[],false);
+//     expect(ship1.isHit([4,4])).toEqual([4,4]);
+// })
+// test('hit mechanics - 4 length miss',()=>{
+//     const ship1=new shipFactory([4,4],[1,4],[],false);
+//     expect(ship1.isHit([4,6])).toEqual('not hit');
+// })
+// test('hit mechanics - 1 length miss',()=>{
+//     const ship1=new shipFactory([2,2],[2,2],[],false);
+//     expect(ship1.isHit([4,4])).toEqual('not hit');
+// })
+// test('hit mechanics - 1 length hit',()=>{
+//     const ship1=new shipFactory([2,2],[2,2],[],false);
+//     expect(ship1.isHit([2,2])).toEqual([2,2]);
+// })
+// test('hit mechanics - 6 length vertical hit',()=>{
+//     const ship1=new shipFactory([6,6],[6,1],[],false);
+//     expect(ship1.isHit([6,2])).toEqual([6,2]);
+// })
+// test('hit mechanics - 6 length vertical hit reversed start and end coords',()=>{
+//     const ship1=new shipFactory([6,1],[6,6],[],false);
+//     expect(ship1.isHit([6,2])).toEqual([6,2]);
+// })
+// test('hit mechanics - check if hit position of the ship was changed by isHit() function',()=>{
+//     const ship1=new shipFactory([6,1],[6,6],[],false);
+//     expect(ship1.isHit([6,2])).toEqual([6,2]);
+//     expect(ship1.hitPosition[0]).toEqual([6,2]);
+// })
 })
